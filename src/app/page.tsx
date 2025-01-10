beda40104a976f84ab912b1f3216e3b8830d1990
xzzxyz <xzzxyz@mail.tsinghua.edu.cn>
2025-01-10 11:24:05 +0800

    25/1/10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,7 +110,24 @@ function SearchParamsComponent() {
             1 ? (
             // isLoading && !collections.length ? (
               <div className="flex flex-1 items-center justify-center">
-                <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary"></div>
+                {/* <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary"></div> */}
+                <div className="h-12 w-12">
+                <div id="loading-box">
+    <div class="loading-left-bg"></div>
+    <div class="loading-right-bg"></div>
+    <div class="spinner-box">
+      <div class="loader">
+        <div class="inner one"></div>
+        <div class="inner two"></div>
+        <div class="inner three"></div>
+      </div>
+      <div class="loading-word">
+        <p class="loading-title" id="loading-title"><%- title %></p>
+        <span id="loading-text">加载中</span>
+      </div>
+    </div>
+  </div>
+                </div>
               </div>
             ) :
               selectedCollectionId || collectionSlug ? (
